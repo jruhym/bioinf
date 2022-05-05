cc761dc542cc4f6627039889293046a25084b0d5
Andrew Heim <aheim@usciences.edu>
2022-05-05 19:03:18 -0400

    Remove deprecation and add Residue and Receptor.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 from .pdb import PDBHelixLine, PDBAtomLine, parse_pdb_HELIX_line, \
-	parse_pdb_ATOM_line, print_pdb_ATOM_line, PDBParser, \
+	parse_pdb_ATOM_line, print_pdb_ATOM_line, \
 	get_pdb_resolution_from_web, pdb_rsln
 from .funcs import parse_prot_cntct_line, float_list, get_ranges, ContactEnd, \
 unionize_distro_given_x, unionize_2_distros, InterresidueContact
-from .deprecated import what_res_pos
-from .constants import aminoacids
+from .constants import aminoacids, Receptor
